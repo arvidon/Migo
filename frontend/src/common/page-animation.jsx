29ae3ad84f557eea207e2e69f8bafe835ceebc25
arvidon <songarashagun@gmail.com>
2026-04-01 12:41:32 +0530

    feat: start

---
+++ frontend/src/common/page-animation.jsx
@@ -0,0 +1,11 @@
+import {AnimatePresence, motion} from "framer-motion"
+
+const AnimationWrapper = ({children}) => {
+    return(
+        <motion.div>
+            {children}
+        </motion.div>
+    )
+}
+
+export default AnimationWrapper
