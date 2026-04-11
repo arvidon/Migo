e699b12c33686f261c81228b3bc48526f8d75aae
arvidon <songarashagun@gmail.com>
2026-04-12 05:07:24 +0530

    editor in making

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -3,31 +3,72 @@ import logo from "../imgs/logo.png"
 import AnimationWrapper from "../common/page-animation"
 import defaultBanner from "../imgs/banner.png"
 import { uploadImage } from "../common/cloudflare"
-import {useRef} from "react"
-
+import {useContext, useEffect} from "react"
+import {Toaster, toast} from "react-hot-toast"
+import { EditorContext } from "../pages/editor.pages"
+import EditorJS from "@editorjs/editorjs"
+import { tools } from "./tools.component"
 
 const BlogEditor = () => {
-    const blogBannerRef = useRef()
-    
+    //const blogBannerRef = useRef()
+    let { blog, blog: {title, banner, content, tags, des}, setBlog } = useContext(EditorContext)
+
+    useEffect(() => {
+        const editor = new EditorJS({
+            holder: 'textEditor',
+            placeholder: "Let's write an awesome story",
+            data: '',
+            tools: tools
+        })
+    }, [])
+
     const handleBannerUpload = (e) => {
         let img = e.target.files[0]
 
         if (img) {
+            let loadingToast = toast.loading("uploading...")
             uploadImage(img).then((url) => {
                 if (url) {
-                    blogBannerRef.current.src = url
+                    toast.dismiss(loadingToast)
+                    toast.success("Uploaded")
+    
+ 
+                    setBlog({...blog, banner: url})
                 }
+            }).catch(err => {
+                toast.dismiss(loadingToast)
+                return toast.error(err)
             })
         }
     }
+
+    const handleTitleKeyDown = (e) => {
+        if(e.keyCode == 13){
+            e.preventDefault()
+        }
+    }
+
+    const handleTitleChange = (e) => {
+        let input = e.target
+        input.style.height = 'auto'
+        input.style.height = input.scrollHeight + 'px'
+
+        setBlog({ ...blog, title: input.value})
+    }
+
+    const handleError = () => {
+        let img = e.target
+        img.src = defaultBanner
+    }
+
     return(
         <>
             <nav className = 'navbar'>
                 <Link to = '/' className="flex-none w-10">
                     <img src={logo}/>
                 </Link>
-                <p className="max-md: hidden text-black line-clamp-1 w-full">
-                    New Blog
+                <p className="max-md:hidden text-black line-clamp-1 w-full">
+                    {title.length ? title : "New Blog"}
                 </p>
 
                 <div className="flex gap-4 ml-auto">
@@ -39,6 +80,7 @@ const BlogEditor = () => {
                     </button>
                 </div>
             </nav>
+            <Toaster />
 
             <AnimationWrapper>
                 <section>
@@ -47,9 +89,10 @@ const BlogEditor = () => {
                         <div className="relative aspect-video hover:opacity-80 bg-white border-2 border-grey">
                             <label htmlFor="uploadBanner">
                                 <img 
-                                    ref={blogBannerRef}
-                                    src={defaultBanner} 
+                                    //ref={blogBannerRef}
+                                    src={banner} 
                                     className="z-20"
+                                    onError={handleError}
                                 />
 
                                 <input 
@@ -62,6 +105,17 @@ const BlogEditor = () => {
                             </label>
                         </div>
 
+                        <textarea
+                            placeholder="Blog Title"
+                            className="text-4xl font-medium w-full h-20
+                            outline-none resize-none mt-10 leading-tight
+                            placeholder:opacity-40" 
+                            onKeyDown={handleTitleKeyDown}
+                            onChange={handleTitleChange}                    
+                        ></textarea>
+
+                        <hr className="w-full opacity-10 my-5"/>
+                        <div id="textEditor" className="font-gelasio"></div>
                     </div>
                 </section>
             </AnimationWrapper>
--- frontend/src/pages/editor.pages.jsx
+++ frontend/src/pages/editor.pages.jsx
@@ -3,16 +3,34 @@ import { Navigate } from "react-router-dom"
 import { userContext } from "../App"
 import BlogEditor from "../components/blog-editor.component"
 import PublishForm from "../components/publish-form.component"
+import { createContext } from "react"
+
+ const blogStructure = {
+        title: '',
+        banner: '',
+        content: [],
+        tags: [],
+        des: '',
+        author: {personal_info: { }}
+}
+
+export const EditorContext = createContext({ })
 
 const Editor = () => {
-
+    const [blog, setBlog] = useState(blogStructure)
     const [editorState, setEditorState] = useState("editor")
 
     const { userAuth } = useContext(userContext)
 
-    return !userAuth?.access_token
-        ? <Navigate to="/signin" />
-        : editorState == "editor" ? <BlogEditor /> : <PublishForm/>
+    return ( 
+        <EditorContext.Provider value={{blog, setBlog, editorState, setEditorState}}>
+            {
+                !userAuth?.access_token
+                ? <Navigate to="/signin" />
+                : editorState == "editor" ? <BlogEditor /> : <PublishForm/>
+            }
+        </EditorContext.Provider>
+    )
 }
 
 export default Editor
